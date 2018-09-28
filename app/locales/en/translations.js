c7f2dbdf6d92326929019a2f99cd28421c70667b
Isaac Ezer <isaac.ezer@gmail.com>
2018-09-28 12:13:03 +0200

    Add ember-i18n

---
+++ app/locales/en/translations.js
@@ -0,0 +1,14 @@
+export default {
+  'user.edit.title': 'Edit User',
+  'user.followers.title.one': 'One Follower',
+  'user.followers.title.other': 'All {{count}} Followers',
+
+  // nested objects work just like dotted keys
+  'button': {
+    'add_user': {
+      'title': 'Add a user',
+      'text': 'Add',
+      'disabled': 'Saving...'
+    }
+  }
+};
